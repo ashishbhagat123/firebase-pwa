e88e77382342b21e823f100e9f3f0618eb822ad0
Ashish bhagat <67907382+ashishbhagat123@users.noreply.github.com>
2024-09-12 19:56:59 +0530

    Update useFCMToken.js

--- utils/hooks/useFCMToken.js
+++ utils/hooks/useFCMToken.js
@@ -5,63 +5,53 @@ import firebaseApp from "../firebase/firebase";
 const useFcmToken = () => {
   const [token, setToken] = useState("");
   const [notificationPermissionStatus, setNotificationPermissionStatus] =
-    useState("");
+    useState("Initial");
 
   const [loading, setLoading] = useState(false);
+  const [support, setSupport] = useState(false);
 
   useEffect(() => {
     const retrieveToken = async () => {
       try {
-        setLoading(true);
         if ("serviceWorker" in navigator && "Notification" in window) {
-          navigator.serviceWorker
-            .register("/firebase-messaging-sw.js")
-            .then(async (registration) => {
-              console.log(
-                "Registration successful, scope is:",
-                registration.scope
-              );
-
-              try {
-                const messaging = getMessaging(firebaseApp);
-                const permission = await Notification.requestPermission();
-
-                setNotificationPermissionStatus(permission);
-
-                if (permission === "granted") {
-                  const currentToken = await getToken(messaging, {
-                    vapidKey:
-                      "BEUXxu90-fjpE5w-lX49fvKKqcq6wGMF805YSeT1pLvWcuO-g_iNI7LSlJrrbFwT4dIuNJN2bgra2GyWwbl2U1g",
-                  });
-                  setLoading(false);
-                  if (currentToken) {
-                    setToken(currentToken);
-                  } else {
-                    console.log(
-                      "No registration token available. Request permission to generate one."
-                    );
-                  }
-                } else if (permission === "denied") {
-                  setLoading(false);
-                }
-              } catch (error) {
-                console.log(error);
-                setLoading(false);
-                alert(error);
+          setLoading(true);
+          setSupport(true);
+          try {
+            await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+            const messaging = getMessaging(firebaseApp);
+            const permission = await Notification.requestPermission();
+            setNotificationPermissionStatus(permission);
+            if (permission === "granted") {
+              const currentToken = await getToken(messaging, {
+                vapidKey:
+                  "BEUXxu90-fjpE5w-lX49fvKKqcq6wGMF805YSeT1pLvWcuO-g_iNI7LSlJrrbFwT4dIuNJN2bgra2GyWwbl2U1g",
+              });
+              setLoading(false);
+              if (currentToken) {
+                setToken(currentToken);
+              } else {
+                console.log(
+                  "No registration token available. Request permission to generate one."
+                );
               }
-            })
+            } else if (permission === "denied") {
+              setLoading(false);
+            }
+          } catch (error) {
+            alert(error);
+            setLoading(false);
+          }
         }
       } catch (error) {
-        console.log("An error occurred while retrieving token:", error);
-        setLoading(false);
         alert(error);
+        setLoading(false);
       }
     };
 
     retrieveToken();
   }, []);
 
-  return { fcmToken: token, notificationPermissionStatus, loading };
+  return { fcmToken: token, notificationPermissionStatus, loading, support };
 };
 
 export default useFcmToken;
