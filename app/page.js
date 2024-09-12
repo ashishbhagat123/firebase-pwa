cb0b8d556c82b43ba9968a6808bfd530cda6ffd0
Ashish bhagat <67907382+ashishbhagat123@users.noreply.github.com>
2024-09-12 19:18:18 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,12 +11,14 @@ import { useServiceWorker } from "@/utils/hooks/useServiceWorker";
 export default function Home() {
   const { fcmToken, notificationPermissionStatus, loading } = useFcmToken();
   // Use the token as needed
-  fcmToken && console.log("FCM token:", fcmToken);
+  console.log("FCM token:", fcmToken);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     if ("serviceWorker" in navigator) {
+    
       const messaging = getMessaging(firebaseApp);
+      console.log("here....M", messaging)
       const unsubscribe = onMessage(messaging, (payload) => {
         // Handle the received push notification while the app is in the foreground
         // You can display a notification or update the UI based on the payload
@@ -39,6 +41,8 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
+      <p>Notification Permission</p>
+      <p>{notificationPermissionStatus}</p>
       {loading ? (
         <p>Fetching Token</p>
       ) : (
