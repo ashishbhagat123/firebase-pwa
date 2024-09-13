879ec00a0957788838ee9c30b2320b47831de793
Ashish bhagat <67907382+ashishbhagat123@users.noreply.github.com>
2024-09-13 06:55:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,48 +4,88 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import firebaseApp from "@/utils/firebase/firebase";
 import { getMessaging, onMessage } from "firebase/messaging";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFcmToken from "@/utils/hooks/useFCMToken";
 import { useServiceWorker } from "@/utils/hooks/useServiceWorker";
 
 export default function Home() {
-  const {
-    fcmToken,
-    notificationPermissionStatus,
-    loading,
-    support,
-    setAskPermission,
-  } = useFcmToken();
+  const { fcmToken, notificationPermissionStatus, loading, setAskPermission } =
+    useFcmToken();
   // Use the token as needed
   console.log("FCM token:", fcmToken);
+  const [supported, setSupported] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     if ("serviceWorker" in navigator && "Notification" in window) {
+      setSupported(true);
       const messaging = getMessaging(firebaseApp);
-      const unsubscribe = onMessage(messaging, (payload) => {
-        // Handle the received push notification while the app is in the foreground
-        // You can display a notification or update the UI based on the payload
-      });
+      const unsubscribe = onMessage(messaging, (payload) => {});
+
+      const handler = (e) => {
+        // Prevent the mini-infobar from appearing on mobile
+        e.preventDefault();
+        // Save the event so it can be triggered later
+        setDeferredPrompt(e);
+        // Make the button visible
+        setIsVisible(true);
+      };
+
+      window.addEventListener("beforeinstallprompt", handler);
+
       return () => {
         unsubscribe(); // Unsubscribe from the onMessage event
+        window.removeEventListener("beforeinstallprompt", handler);
       };
     }
   }, []);
 
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      // Clear the saved prompt
+      setDeferredPrompt(null);
+      // Hide the button
+      setIsVisible(false);
+    }
+  };
+
   return (
     <div className={styles.page}>
-      <p>Notification Permission</p>
-      <p>{notificationPermissionStatus}</p>
+      <p>
+        Notification Permission: {notificationPermissionStatus?.toUpperCase()}{" "}
+      </p>
       {loading ? (
         <p>Fetching Token</p>
       ) : (
         <p className={styles.token}>{fcmToken}</p>
       )}
-      {support ? "support success" : "support failed"}
-      <button onClick={() => setAskPermission((prev) => !prev)}>
-        SetPermission
+      {supported ? "Support success" : "Support failed"}
+      <button
+        className={styles.btn}
+        onClick={() => setAskPermission((prev) => !prev)}
+      >
+        Enable Notification
       </button>
+      {isVisible && (
+        <button
+          onClick={handleInstallClick}
+          className= {styles.pwaCta}
+        >
+          Install PWA
+        </button>
+      )}
+
     </div>
   );
 }
